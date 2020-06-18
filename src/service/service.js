e724266a12ec91306d4e6afd18b3f03b2315444d
novitckas <novitckas@selectel.ru>
2020-06-18 20:16:37 +0300

    Скрипт для генерации данных

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,66 @@
 'use strict';
 
-console.log(`Hello, world!`);
+const fs = require(`fs`);
+const {version} = require(`../../package.json`);
+const constants = require(`./constants`);
+const {getRandomNumber, threeMonthsFromNowInMS} = require(`./utils`);
+
+// const argv = process.argv;
+const [commandName = ``, commandValue = null] = process.argv.slice(2);
+
+const runCommand = (name, value) => {
+  switch (name) {
+    case `--generate`:
+      generateOffer(value);
+      break;
+    case `--version`:
+      showVersion();
+      break;
+    default:
+      showHelp();
+  }
+};
+
+const showHelp = () => {
+  console.log(constants.helpText);
+};
+
+const showVersion = () => {
+  console.log(`v${version}`);
+};
+
+const generateOffer = (value) => {
+  const objectInArrayNumber = Number.parseInt(value, 10) || constants.DEFAULT_NUMBER;
+  if (objectInArrayNumber > constants.MAX_COUNT) {
+    console.log(`Не больше 1000 публикаций`);
+    process.exit(1);
+  }
+  const resultArray = [];
+  for (let i = 0; i < objectInArrayNumber; i++) {
+    resultArray.push(generateMockedObject());
+  }
+  fs.writeFile(`./mock.json`, JSON.stringify(resultArray), (err) => {
+    if (err) {
+      process.exit(1);
+    }
+    process.exit(0);
+  });
+};
+
+const randomSliceArray = (arrayName, maxLength) => {
+  const start = getRandomNumber(0, constants[arrayName].length - 1);
+  const end = getRandomNumber(start + 1, maxLength ? start + 1 + maxLength : constants[arrayName].length);
+  return constants[arrayName].slice(start, end);
+};
+
+const generateMockedObject = () => {
+  return {
+    title: constants.titleList[getRandomNumber(0, constants.titleList.length)],
+    createdDate: new Date(getRandomNumber(threeMonthsFromNowInMS, +new Date())),
+    announce: randomSliceArray(`textList`, constants.SENTENCES_IN_ANNOUNCE_MAX),
+    fullText: randomSliceArray(`textList`, constants.textList.length),
+    category: randomSliceArray(`categoryList`)
+  };
+};
+
+runCommand(commandName, commandValue);
