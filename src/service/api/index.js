c8296d3670ea4537835b4b048b369ff8769df586
novitckas <novitckas@selectel.ru>
2020-10-18 22:56:09 +0300

    Обработка эндпоинтов поиска и категорий

--- src/service/api/index.js
+++ src/service/api/index.js
@@ -3,13 +3,13 @@
 const {Router} = require(`express`);
 
 const articleRouter = require(`./articles`);
-// const categoryRouter = require(`./category`);
-// const searchRouter = require(`./search`);
+const categoryRouter = require(`./category`);
+const searchRouter = require(`./search`);
 const {getMockedData, getMockedCategoryList} = require(`../lib/get-mocked-data`);
 const ArticleService = require(`../data-service/article`);
-// const CategoryService = require(`../data-service/category`);
+const CategoryService = require(`../data-service/category`);
 const CommentService = require(`../data-service/comment`);
-// const SearchService = require(`../data-service/search`);
+const SearchService = require(`../data-service/search`);
 
 const app = new Router();
 
@@ -17,8 +17,8 @@ const app = new Router();
   const mockedData = await getMockedData();
   const mockedCategoryList = await getMockedCategoryList();
   app.use(`/articles`, articleRouter(new ArticleService(mockedData), new CommentService()));
-  // app.use(`/categories`, categoryRouter(new CategoryService(mockedCategoryList)));
-  // app.use(`/search`, searchRouter(new SearchService(mockedData)));
+  app.use(`/categories`, categoryRouter(new CategoryService(mockedCategoryList)));
+  app.use(`/search`, searchRouter(new SearchService(mockedData)));
 })();
 
 module.exports = app;
